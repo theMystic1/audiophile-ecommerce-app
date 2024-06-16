4f1682c748ca154181b541ff96c89dec474d6f8f
themystic1 <cluckygo@gmail.com>
2024-06-16 04:14:25 +0100

    fixed the localstorage issue

--- app/_lib/useLocalStorage.js
+++ app/_lib/useLocalStorage.js
@@ -1,17 +1,33 @@
 import { useState, useEffect } from "react";
 
 export function useLocalStorageState(initialState, key) {
-  const [value, setValue] = useState(function () {
-    const storedValue = localStorage?.getItem(key);
-    return storedValue ? JSON.parse(storedValue) : initialState;
+  const isClient = typeof window !== "undefined";
+
+  const [value, setValue] = useState(() => {
+    if (isClient) {
+      const storedValue = localStorage.getItem(key);
+      return storedValue ? JSON.parse(storedValue) : initialState;
+    } else {
+      return initialState;
+    }
   });
 
-  useEffect(
-    function () {
-      localStorage?.setItem(key, JSON.stringify(value));
-    },
-    [value, key]
-  );
+  const setStoredValue = (newValue) => {
+    if (isClient) {
+      try {
+        localStorage.setItem(key, JSON.stringify(newValue));
+        setValue(newValue);
+      } catch (error) {
+        console.error("Failed to store value in localStorage", error);
+      }
+    }
+  };
 
-  return [value, setValue];
+  useEffect(() => {
+    if (isClient) {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
+  }, [value, key, isClient]);
+
+  return [value, setStoredValue];
 }
